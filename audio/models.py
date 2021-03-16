61a599126bafff552a6c4214ab277ae803176651
MS List <simlist@gmail.com>
2021-03-15 21:31:20 -0400

    Created recordings model.

--- audio/models.py
+++ audio/models.py
@@ -1,14 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
 class Author(models.Model):
     name = models.CharField(max_length=50)
+    slug = models.SlugField()
 
     def __str__(self):
         return self.name
 
-class Series(models.model):
+
+class Series(models.Model):
     name = models.CharField(max_length=40)
+    slug = models.SlugField()
 
     def __str__(self):
         return self.name
+
+
+class Recording(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    series = models.ForeignKey(Series, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    slug = models.SlugField()
+    date = models.DateField()
+    audio = models.FileField(upload_to='recordings/')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-date',]
